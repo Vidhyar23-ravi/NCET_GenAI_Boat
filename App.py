03512875f729830ea08751318383f9fb6b68e9eb
Vidhyar23-ravi <rvidhya2004@gmail.com>
2026-04-15 10:38:47 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,6 @@
+import streamlit as st
+from groq import Groq
 
+st.set_page_config("pragyanAI Content Generator",layout="wide")
+st.title("pragyanAI - Content Generator")
+st.image("pragyanAI_transperent.jpg")
